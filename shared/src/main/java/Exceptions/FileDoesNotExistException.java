ef957a5d4e0a143d5ab71893128bff10a1102ec1
Mor <mormerhav@gmail.com>
2015-09-06 11:23:16 +0300

    Forgotten files Thumbnail.java and FilesDoesNotExistException.java

---
+++ shared/src/main/java/Exceptions/FileDoesNotExistException.java
@@ -0,0 +1,11 @@
+package Exceptions;
+
+/**
+ * Created by Mor on 06/09/2015.
+ */
+public class FileDoesNotExistException extends Exception {
+
+    public FileDoesNotExistException(String msg) {
+        super(msg);
+    }
+}
