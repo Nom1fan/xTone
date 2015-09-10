a902b7513a5eccb62a0dad9dcdd30488dbbd12d2
Mor <mormerhav@gmail.com>
2015-09-10 19:27:18 +0300

    Server - Improving server to be more robust LogsManager - Adding some prints

--- server_side/src/main/java/com/server_side/Server.java
+++ server_side/src/main/java/com/server_side/Server.java
@@ -11,45 +11,59 @@ import ServerObjects.ConnectionToClient;
 
 public class Server {
 
-    private ClientsManager clientsManager;
-    private static Logger logger = null;
-    private static boolean cont = true;
+    private ClientsManager _clientsManager;
+    private static Logger _logger = null;
+    private static int _countFailures = 0;
+    private static final int MAX_FAILURES = 20;
+    private ServerSocket _serverSocket;
 
-    public Server() throws IOException {
+    public Server()  {
 
-        LogsManager.createServerLogsDir();
-        LogsManager.clearLogs();
-        logger = LogsManager.getServerLogger("Server");
-        logger.info("Starting server...");
-        System.out.println("Starting server...");
+        try {
+            LogsManager.createServerLogsDir();
+            LogsManager.clearLogs();
+            _logger = LogsManager.getServerLogger("Server");
+            _clientsManager = new ClientsManager();
 
-        ServerSocket serverSocket = new ServerSocket(SharedConstants.PORT);
+            System.out.println("Starting server...");
+            _serverSocket = new ServerSocket(SharedConstants.PORT);
+            System.out.println("Server running on port:" + SharedConstants.PORT + "...");
+            _logger.info("Server running on port:" + SharedConstants.PORT + "...");
 
-        logger.info("Server running on port:"+SharedConstants.PORT+"...");
-        System.out.println("Server running on port:"+SharedConstants.PORT+"...");
-
-        clientsManager = new ClientsManager();
-
-        while(cont)
-        {
-            Socket clientSocket = serverSocket.accept();
-            System.out.println("Receiving client connection...");
-            ConnectionToClient ctc = new ConnectionToClient(clientSocket);
-            new ServerThread(ctc).start();
+            listen();
         }
+        catch (IOException e ) {
+            e.printStackTrace();
+            _logger.severe("Failed to initialize server components. Exception:"+e.getMessage());
+        }
+
+    }
+
+    private void listen() {
+
+        /* Listen loop */
+        while(_countFailures<MAX_FAILURES)
+        {
+            try {
+
+                Socket clientSocket = _serverSocket.accept();
+                System.out.println("Receiving client connection...");
+                ConnectionToClient ctc = new ConnectionToClient(clientSocket);
+                new ServerThread(ctc).start();
+            }
+            catch(IOException e) {
+                _countFailures++;
+                e.printStackTrace();
+                _logger.severe("Failed to receive client connection. # of Failures:" + _countFailures + " Exception:" + e.getMessage());
+            }
+        }
+        _logger.severe("Server terminating after "+_countFailures+" failures");
     }
 
 
     public static void main(String[] args) {
 
-        try {
-            new Server();
-
-        } catch (IOException e) {
-
-            e.printStackTrace();
-            cont = false;
-        }
+       new Server();
     }
 }
 
