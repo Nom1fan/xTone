b24da8cb72d02eb07ad51309aced46d1865b50d8
rony <ronyahae@gmail.com>
2016-03-29 16:09:46 +0300

    videoIntent.putExtra( MediaStore.EXTRA_DURATION_LIMIT, 30); // set video record time to 30 seconds         videoIntent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 0); // set the video image quality to low

--- specialCall/src/main/java/com/ui/activities/SelectMediaActivity.java
+++ specialCall/src/main/java/com/ui/activities/SelectMediaActivity.java
@@ -182,6 +182,10 @@ public class SelectMediaActivity extends Activity implements View.OnClickListene
         final Intent videoIntent = new Intent(
                 MediaStore.ACTION_VIDEO_CAPTURE);
         videoIntent.putExtra(MediaStore.EXTRA_OUTPUT, _outputFileUri);
+        videoIntent.putExtra( MediaStore.EXTRA_DURATION_LIMIT, 30);
+        videoIntent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 0); // set the video image quality to high
+
+
         startActivityForResult(videoIntent, code); // // TODO rony: 31/01/2016 see native camera opens and not other weird different cameras
     }
 
@@ -256,7 +260,8 @@ public class SelectMediaActivity extends Activity implements View.OnClickListene
                 }
 
                 fm = new FileManager(path);
-                Log.i(TAG, "file selected: " + path );
+                Log.i(TAG, "file selected: " + path + " File Size: " + String.valueOf(fm.getFileSize()) );
+
                 Intent i = new Intent(getApplicationContext(), StorageServerProxyService.class);
                 i.setAction(StorageServerProxyService.ACTION_UPLOAD);
                 i.putExtra(StorageServerProxyService.DESTINATION_ID, _destPhoneNumber);
