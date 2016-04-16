24cd681017d6c2627cecc6853186ccb762b6ec8a
rony <ronyahae@gmail.com>
2016-04-16 16:29:06 +0300

    adding actionbar for the Drawer Settings Pages

--- specialCall/src/main/java/com/ui/activities/SetAboutHelpActivity.java
+++ specialCall/src/main/java/com/ui/activities/SetAboutHelpActivity.java
@@ -1,20 +1,43 @@
 package com.ui.activities;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import com.mediacallz.app.R;
 
 /**
  * Created by Mor on 31/03/2016.
  */
-public class SetAboutHelpActivity extends Activity {
+public class SetAboutHelpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
 
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+            actionBar.setCustomView(R.layout.custom_action_bar);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
         getFragmentManager().beginTransaction().replace(android.R.id.content,
                 new AboutAndHelp()).commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return true;
     }
 
 }
