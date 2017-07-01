8c2f22872b677e0bed0a99b0f312688d908f40f5
Mor <mormerhav@gmail.com>
2017-07-01 18:44:28 +0300

    Fixing server address

--- specialCall/src/main/java/com/data/objects/Constants.java
+++ specialCall/src/main/java/com/data/objects/Constants.java
@@ -65,8 +65,8 @@ public abstract class Constants {
     //endregion
 
     //region Connection to server
-    public static final String SERVER_HOST = "epicall.no-ip.biz";
-    public static final int SERVER_PORT = 8888;
+    public static final String SERVER_HOST = "server.mediacallz.com";
+    public static final int SERVER_PORT = 8080;
     //endregion
 
     //region Batch
